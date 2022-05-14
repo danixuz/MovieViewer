769b78a9f77376a06c0556647c63d4dfc1b20160
danixuz <danixuz@icloud.com>
2022-05-15 02:08:15 +0300

    Initial Commit

---
+++ MovieViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieViewer
+//
+//  Created by Daniel Spalek on 15/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieViewer/MovieViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieViewerApp.swift
+//  MovieViewer
+//
+//  Created by Daniel Spalek on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
